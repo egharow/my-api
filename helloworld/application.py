4c14a3885101deccd4fe780aab656f116fdf0fab
egharow <elyharow@gmail.com>
2023-05-02 06:35:40 +0000

    get bitcoin currency from api

--- helloworld/application.py
+++ helloworld/application.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+import requests
 
 application = Flask(__name__)
 
@@ -17,11 +18,20 @@ def post():
 def get_currency_check(currency):
     return Response(json.dumps({'result': currency_rate.get(currency) }), mimetype='application/json', status=200)
 
+@application.route('/check/bitcoin/<currency>', methods=['GET'])
+def get_bitcoin(currency):
+    res = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json();
+    dic = res.get('bpi')
+    value= dic.get(currency)
+    #print(res.text)
+    return Response(json.dumps(value), mimetype='application/json', status=200)
 
 currency_rate = {
  'usd' : 3.3,
  'pound' : 4.5,
  'euro' : 4.8}
+
+
 
 
 if __name__ == '__main__':
